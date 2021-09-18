7184effc58f9ae8a08cc87510feef1b19ea7269e
Henrique Pagno de Lima <henriquepagno@gmail.com>
2021-09-18 11:19:24 -0300

    Fix: Made a refactor of the site layout.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -23,6 +23,10 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@300;400;500;600&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
