6cd94172b9db122b0b79285af6a629c8f9a916f9
Henrique Pagno de Lima <henriquepagno@gmail.com>
2021-10-30 16:01:11 -0300

    feat: Adjustments to deploy project.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333/',
+  baseURL:
+    process.env.NODE_ENV !== 'production'
+      ? 'http://localhost:3333/'
+      : 'https://comparador-renda-fixa-backend.herokuapp.com/',
 });
 
 export default api;
